f3394d8a478bc2e92bbdf29dd5e6b66c04aab69d
ilissavalentine <ilissa.valentine@gmail.com>
2025-01-25 14:31:38 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Name: Ilissa Valentine
+
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hola mundo");
+                System.out.println("Job Title: Computer and Information Research Scientist");
+        System.out.println("Why: I can apply what I know and have a job for a while.");
+        System.out.println("Median salary: $145,080");
+
+
+    }
+}
